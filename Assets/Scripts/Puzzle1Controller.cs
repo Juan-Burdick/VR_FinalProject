dced0091e191a645a4a38a6bf26f208b42f0ca2a
Juan Burdick <burdick@andrews.edu>
2017-12-08 20:59:59 -0500

    Finished puzzle 1?

---
+++ Assets/Scripts/Puzzle1Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1Controller : MonoBehaviour {
+
+	private GameObject mWall;
+
+	// Use this for initialization
+	void Start () {
+		mWall = transform.Find("Puzzle1_Wall").gameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (mWall.transform.position.y <= -1000) {
+			// Delete invisible walls
+		}
+	}
+}
